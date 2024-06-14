19999d686f8835575f32bd9ab3ec4c3cc7d9b299
Saarxp <saar772@gmail.com>
2024-06-14 13:09:20 +0300

    adding snowboard icon + text on about me

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,11 +1,12 @@
 import style from "./about.module.scss";
 import headerImage from "../../assets/images/web-development-image-01.png";
 import { GiConsoleController } from "react-icons/gi";
-import { VscFileBinary } from "react-icons/vsc";
 import { ImEarth } from "react-icons/im";
+import { PiPersonSimpleSnowboardThin } from "react-icons/pi";
 import SkillsContainer from "./skillsContainer/SkillsContainer";
 import MyTools from "./myTools/MyTools";
 import Footer from "../footer/Footer";
+
 
 function About() {
   return (
@@ -19,12 +20,13 @@ function About() {
           <div className={style.aboutTextWrapper}>
             <p>Hi, I’m Saar, a Full Stack Developer who loves creating smooth, user-friendly web applications. 
               I studied at iNT College - Institute of Innovation & Technology and I'm always excited to learn new things.</p>
+              <p>Apart from coding, some other activities that I love to do!</p>
             <ul>
               <li>
                 <GiConsoleController /> Playing Games
               </li>
               <li>
-                <VscFileBinary /> Code
+              <PiPersonSimpleSnowboardThin /> Snowboarding
               </li>
               <li>
                 <ImEarth /> Travelling
